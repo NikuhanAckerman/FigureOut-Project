c41b906628c6a96b3eba8f9d80659c151c85cc57
Satirico <nicolassatil81@gmail.com>
2024-08-14 19:26:00 -0300

    Project initial setup commit.
    Initial setup for the project.

---
+++ figureout/src/main/java/com/project/figureout/FigureoutApplication.java
@@ -0,0 +1,13 @@
+package com.project.figureout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigureoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigureoutApplication.class, args);
+	}
+
+}
