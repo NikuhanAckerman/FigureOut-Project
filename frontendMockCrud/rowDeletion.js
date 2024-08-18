d04049b9cbb49b228f8259dda784b5be0ac5554d
Satirico <nicolassatil81@gmail.com>
2024-08-17 23:46:50 -0300

    Fixing bug with the "Excluir" button
    Bug where clicking on the button doesn't delete the row

--- frontendMockCrud/rowDeletion.js
+++ frontendMockCrud/rowDeletion.js
@@ -1,19 +1,12 @@
 
 function deleteRow() {
-
     const table = document.getElementById('client-table');
-
-
     table.addEventListener('click', function(event) {
         if (event.target && event.target.classList.contains('btn-delete')) {
             const row = event.target.closest('tr');
             row.remove();
         }
     });
-}
-
-function updateRow() {
-
 }
 
 document.addEventListener('DOMContentLoaded', deleteRow);
