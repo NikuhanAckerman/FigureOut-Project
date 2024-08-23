b91763d0d4687b4936449035c563b9c0ee8c7ca9
Satirico <nicolassatil81@gmail.com>
2024-08-23 20:08:23 -0300

    Attempting to use Thymeleaf to show client list

--- figureout/src/main/java/com/project/figureout/controller/ClientController.java
+++ figureout/src/main/java/com/project/figureout/controller/ClientController.java
@@ -3,27 +3,31 @@ package com.project.figureout.controller;
 import com.project.figureout.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.project.figureout.model.Client;
 
 import java.util.Optional;
 
 @Controller
-@RequestMapping(path = "/clientes")
+@RequestMapping(path = "/index")
 public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
 
-    @PostMapping()
+    @PostMapping("/criarCliente")
     public Client addClient(@RequestBody Client client) {
         clientRepository.save(client);
         return client;
     }
 
     @GetMapping({"", "/"})
-    public Iterable<Client> getAllClients() {
-        return clientRepository.findAll();
+    public String getAllClients(Model model) {
+
+        Iterable<Client> clients =  clientRepository.findAll();
+        model.addAttribute("clients", clients);
+        return "index";
     }
 
     @GetMapping("/{id}")
@@ -31,12 +35,15 @@ public class ClientController {
         return clientRepository.findById(id);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/atualizarCliente")
     public Client updateClient(@PathVariable long id, @RequestBody Client client) {
-        Optional<Client> clientToChange = getClientById(id);
+        Client clientToChange = getClientById(id).get();
 
-        client.setName(clientToChange.get().getName());
-
+        clientToChange.setName(client.getName());
+        clientToChange.setEmail(client.getEmail());
+        clientToChange.setPhoneNumber(client.getPhoneNumber());
+        clientRepository.save(clientToChange);
+        return clientToChange;
     }
 
 }
