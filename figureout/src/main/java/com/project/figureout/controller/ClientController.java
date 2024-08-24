ea532ab223dc493e39f956d181a3857eb784a629
Satirico <nicolassatil81@gmail.com>
2024-08-24 14:16:40 -0300

    More attempts

--- figureout/src/main/java/com/project/figureout/controller/ClientController.java
+++ figureout/src/main/java/com/project/figureout/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.project.figureout.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import com.project.figureout.model.Client;
 
@@ -17,19 +18,35 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
-    @GetMapping({"", "/"})
+    @GetMapping({"", "/", "/index"})
     public String showClients(Model model) {
 
         Iterable<Client> clients =  clientRepository.findAll();
         model.addAttribute("clients", clients);
-        return "/index";
+        return "index";
     }
 
-    @PostMapping("/createClient")
+    @GetMapping("/createClient")
     public String addClient(Model model) {
         ClientDTO clientDTO = new ClientDTO();
         model.addAttribute("clientDTO", clientDTO);
-        return "/index/createClient";
+        return "createClient";
+    }
+
+    @PostMapping("/createClient")
+    public String handleCreateClient(@ModelAttribute("clientDTO") ClientDTO clientDTO, BindingResult bindingResult) {
+
+
+        Client client = new Client();
+        client.setName(clientDTO.getName());
+        client.setEmail(clientDTO.getEmail());
+        client.setPhoneNumber(clientDTO.getPhoneNumber());
+        client.setCpf(clientDTO.getCpf());
+        client.setAddress(clientDTO.getAddress());
+
+        System.out.println("post called");
+
+        return "redirect:/index";
 
     }
 
--- figureout/src/main/java/com/project/figureout/dto/ClientDTO.java
+++ figureout/src/main/java/com/project/figureout/dto/ClientDTO.java
@@ -2,25 +2,27 @@ package com.project.figureout.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 public class ClientDTO {
 
     @NotEmpty(message = "Digite o nome.")
-    private String name;
+    @Getter @Setter private String name;
 
     @NotEmpty(message = "Digite o email.")
-    private String email;
+    @Getter @Setter private String email;
 
     @NotEmpty(message = "Digite o número de telefone.")
     @Size(min = 15, max = 15)
     // (11) 98224-8652
-    private String phoneNumber;
+    @Getter @Setter private String phoneNumber;
 
     @Size(min = 14, max = 14)
     @NotEmpty(message = "Digite o CPF.")
-    private String cpf;
+    @Getter @Setter private String cpf;
 
     @NotEmpty(message = "Digite o endereço.")
-    private String address;
+    @Getter @Setter private String address;
 
 }
