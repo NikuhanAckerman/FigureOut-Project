4296b11f2760e8eb745d7ab4e90636a3d1855242
Satirico <nicolassatil81@gmail.com>
2024-10-22 16:20:59 -0300

    Revert "Merge branch 'main' of https://github.com/NikuhanAckerman/FigureOut-Project"
    This reverts commit 9f9ce0f29f96870265745604ce523d27a4b5e689, reversing
    changes made to 6fa15f14c550ac6e275014d6fd04b48ef0692f9d.

--- figureout/src/main/java/com/project/figureout/controller/StockController.java
+++ figureout/src/main/java/com/project/figureout/controller/StockController.java
@@ -1,29 +1,21 @@
 package com.project.figureout.controller;
 
 import com.project.figureout.model.Product;
-import com.project.figureout.model.Stock;
-import com.project.figureout.service.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/products")
 public class StockController {
 
-    @Autowired
-    private StockService stockService;
 
-    @GetMapping("/stock")
+
+    /*@GetMapping("/seeProducts")
     public String showStockGet(Model model) {
-        List<Stock> products =  stockService.getAllProductsInStock();
+        List<Product> products =  stockService.getAllProductsInStock();
         model.addAttribute("products", products);
 
-        return "stock";
-    }
+        return "adminSeeProducts";
+    }*/
 
 }
