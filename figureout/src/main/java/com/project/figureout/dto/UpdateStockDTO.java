d8b26e1d9afd133fe2f524541bc483f8bbba2807
JakeFalcs <renanlsilva2021@gmail.com>
2024-11-30 19:16:03 -0300

    Mais testes de funcionalidade

--- figureout/src/main/java/com/project/figureout/dto/UpdateStockDTO.java
+++ figureout/src/main/java/com/project/figureout/dto/UpdateStockDTO.java
@@ -21,7 +21,7 @@ public class UpdateStockDTO {
 
     @PastOrPresent(message = "A data de mudança deve estar no passado ou no presente.")
     @NotNull(message = "O campo de data de mudança não pode ser nulo.")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateChangeOfStockQuantity;
 
     private List<Long> supplier = new ArrayList<>();
